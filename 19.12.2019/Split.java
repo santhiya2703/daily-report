e7891fc376bfd0595335c6fcf991c9b6580f32d1
santhiya2703 <58658243+santhiya2703@users.noreply.github.com>
2019-12-19 11:10:09 +0530

    Create Split.java

---
+++ 19.12.2019/Split.java
@@ -0,0 +1,23 @@
+
+import java.util.regex.Matcher; 
+import java.util.regex.Pattern; 
+  
+class Demo 
+{ 
+    public static void main(String args[]) 
+    { 
+        String text = "s1a2n3t4h5i6y7a8"; 
+  
+        // Specifies the string pattern which is to be searched 
+        String delimiter =  "\\d"; 
+        Pattern pattern = Pattern.compile(delimiter, 
+                                        Pattern.CASE_INSENSITIVE); 
+  
+        // Used to perform case insensitive search of the string 
+        String[] result = pattern.split(text); 
+  
+  
+        for (String temp: result) 
+            System.out.println(temp); 
+    } 
+} 
